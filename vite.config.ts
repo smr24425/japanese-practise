8aa19d3c2228a6fe932cc045d336a2ec6572155a
Arthur <smr24425@gmail.com>
2025-10-14 16:40:17 +0800

    fixed vite base

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 import { VitePWA } from "vite-plugin-pwa";
 
 export default defineConfig({
-  base: "./", //repo name
+  base: "/japanese-practise/", //repo name
   plugins: [
     react(),
     VitePWA({
